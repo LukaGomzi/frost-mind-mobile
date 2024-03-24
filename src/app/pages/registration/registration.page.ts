f7f25d55698bd0119eb83c5b5d44259e1853678c
Luka Gomzi <gomzi.luka77@gmail.com>
2024-03-24 23:54:31 +0100

    feat: Implemented simple happy path registration

--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
   styleUrls: ['./registration.page.scss'],
 })
-export class RegistrationPage implements OnInit {
+export class RegistrationPage {
+  username?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router, private toastController: ToastController) {}
 
-  ngOnInit() {
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'top',
+    });
+    toast.present();
   }
 
+  register() {
+    if (this.password !== this.confirmPassword) {
+      this.presentToast('Passwords do not match.');
+      return;
+    }
+
+    const registerUrl = 'http://localhost:3000/api/v1/user/register';
+    this.http.post(registerUrl, {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: (response) => {
+        this.presentToast('Registration successful. Please log in.');
+        this.router.navigateByUrl('/login');
+      },
+      error: (error) => {
+        console.error('Registration failed', error);
+        this.presentToast('Registration failed.');
+      }
+    });
+  }
 }
