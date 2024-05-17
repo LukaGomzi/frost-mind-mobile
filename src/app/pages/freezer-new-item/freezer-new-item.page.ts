39d4a715250e90589492ba73c8be3ad6840151f6
Luka Gomzi <gomzi.luka77@gmail.com>
2024-05-17 13:00:02 +0200

    feat: new item state management

--- src/app/pages/freezer-new-item/freezer-new-item.page.ts
+++ src/app/pages/freezer-new-item/freezer-new-item.page.ts
@@ -5,7 +5,7 @@ import { FoodType } from "../../services/food-type.service";
 import { FoodTypeStore } from "../../state/food-type.store";
 import { ActivatedRoute, Router } from "@angular/router";
 import { FreezersStore } from "../../state/freezer.store";
-import { FreezerItemRequest } from "../../core/models/freezer-item.model";
+import { NotificationService } from "../../services/notification.service";
 
 @Component({
   selector: 'app-freezer-new-item',
@@ -16,12 +16,14 @@ export class FreezerNewItemPage implements OnInit {
   newItemForm: FormGroup;
   freezerId?: number;
   foodTypes$?: Observable<FoodType[]>;
+  isLoading: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private foodTypeStore: FoodTypeStore,
     private freezersStore: FreezersStore,
+    private notificationsService: NotificationService,
   ) {
     this.newItemForm = new FormGroup({
       name: new FormControl('', Validators.required),
@@ -41,16 +43,21 @@ export class FreezerNewItemPage implements OnInit {
 
   onSubmit() {
     if (this.newItemForm.valid) {
+      this.isLoading = true;
       const itemRequest = {
         ...this.newItemForm.value,
         freezerId: this.freezerId
       };
       this.freezersStore.addItemToFreezer(this.freezerId!, itemRequest).subscribe({
         next: _ => {
+          this.isLoading = false;
+          this.notificationsService.success("Successfully added item to the Freezer.")
           this.router.navigateByUrl('/freezer-details/' + this.freezerId!);
         },
         error: (error) => {
+          this.isLoading = false;
           console.error('Failed to add item', error);
+          this.notificationsService.error("Failed to add item. Please try again.")
         }
       });
     }
