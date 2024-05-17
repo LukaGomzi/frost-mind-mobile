20ac709dd39394a03a67ee83f3346d47c4bff33f
Luka Gomzi <gomzi.luka77@gmail.com>
2024-05-17 11:16:18 +0200

    feat: state managment and notifications for freezer actions

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { combineLatest, first, map, Observable, Subject, takeUntil } from "rxjs"
 import { Router } from "@angular/router";
 import { AlertController } from "@ionic/angular";
 import { FreezersStore } from "../state/freezer.store";
+import { NotificationService } from "../services/notification.service";
 
 @Component({
   selector: 'app-home',
@@ -24,7 +25,8 @@ export class HomePage implements OnInit, OnDestroy {
     private freezerService: FreezerService,
     private freezersStore: FreezersStore,
     private router: Router,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private notificationsService: NotificationService,
   ) {
     this.loadFreezers();
   }
@@ -41,7 +43,7 @@ export class HomePage implements OnInit, OnDestroy {
     this.router.navigateByUrl('/add-freezer');
   }
 
-  async confirmDelete(freezerId: number) {
+  async confirmDelete(freezer: Freezer) {
     const alert = await this.alertController.create({
       header: 'Confirm Delete',
       message: 'Are you sure you want to delete this freezer?',
@@ -53,7 +55,7 @@ export class HomePage implements OnInit, OnDestroy {
         }, {
           text: 'Yes, Delete it',
           handler: () => {
-            this.deleteFreezer(freezerId);
+            this.deleteFreezer(freezer);
           }
         }
       ]
@@ -62,17 +64,19 @@ export class HomePage implements OnInit, OnDestroy {
     await alert.present();
   }
 
-  deleteFreezer(freezerId: number) {
-    this.freezerService.deleteFreezer(freezerId)
+  deleteFreezer(freezer: Freezer) {
+    this.freezerService.deleteFreezer(freezer.id)
       .pipe(
         first(),
         takeUntil(this.onDestroy$),
       )
       .subscribe({
       next: () => {
+        this.notificationsService.success(`Freezer ${freezer.name} deleted successfully.`);
         this.loadFreezers();
       },
       error: (error) => {
+        this.notificationsService.error('Failed to delete the freezer. Please try again later.');
         console.error('Error deleting freezer:', error);
       },
     })
--- src/app/pages/add-freezer/add-freezer.page.ts
+++ src/app/pages/add-freezer/add-freezer.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FreezerService } from '../../core/services/freezer.service';
+import { NotificationService } from "../../services/notification.service";
+import { FreezersStore } from "../../state/freezer.store";
 
 @Component({
   selector: 'app-add-freezer',
@@ -9,19 +11,33 @@ import { FreezerService } from '../../core/services/freezer.service';
 })
 export class AddFreezerPage {
   name?: string;
+  isLoading: boolean = false;
 
-  constructor(private freezerService: FreezerService, private router: Router) {}
+  constructor(
+    private freezerService: FreezerService,
+    private freezerStore: FreezersStore,
+    private router: Router,
+    private notificationsService: NotificationService,
+  ) {}
 
   submit() {
     if (this.name) {
+      this.isLoading = true;
       this.freezerService.addFreezer({ name: this.name }).subscribe({
         next: () => {
+          this.isLoading = false;
+          this.notificationsService.success(`Freezer ${this.name} added successfully.`);
+          this.freezerStore.loadFreezers();
           this.router.navigateByUrl('/tabs/home');
         },
         error: (error) => {
+          this.isLoading = false;
+          this.notificationsService.error('Failed to add freezer. Please try again later.');
           console.error('Error adding freezer:', error);
         },
       });
+    } else {
+      this.notificationsService.error('Please enter a freezer name.');
     }
   }
 }
